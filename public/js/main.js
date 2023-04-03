a1f0cdd78b1c416a68ac1fe8af23cbd199165b22
Lukassza <lukasszafranek@gmail.com>
2023-04-03 15:54:42 +0200

    3

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+let nuit = document.querySelector(".nuit");
+let nav = document.querySelector(".nav");
+
+nuit.addEventListener("click",function(e){
+    e.preventDefault();
+    if
+nav.classList.add("navnuit")
+
+})
