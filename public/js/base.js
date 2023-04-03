9b9a470b770ac96d0d838d05f8c5510e15df65d4
Lukassza <lukasszafranek@gmail.com>
2023-04-03 16:10:26 +0200

    3

---
+++ public/js/base.js
@@ -0,0 +1,11 @@
+let nuit = document.querySelector(".nuit");
+let nav = document.querySelector(".nav");
+let a= document.querySelector("a");
+
+nuit.addEventListener("click",function(e){
+    
+    if(a.classList.contains("anuit")){
+        a.classlist.remove("anuit")
+    }
+    else{a.classList.add("anuit") }
+})
--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-let nuit = document.querySelector(".nuit");
-let nav = document.querySelector(".nav");
-
-nuit.addEventListener("click",function(e){
-    e.preventDefault();
-    if
-nav.classList.add("navnuit")
-
-})
