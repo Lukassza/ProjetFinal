3df44252d29c1c1333d80fe8633742d4f5c9deed
Lukassza <lukasszafranek@gmail.com>
2023-04-04 14:30:32 +0200

    6

--- public/js/base.js
+++ public/js/base.js
@@ -1,11 +1,41 @@
 let nuit = document.querySelector(".nuit");
-let nav = document.querySelector(".nav");
-let a= document.querySelector("a");
+let nav = document.querySelector("nav");
+let a = document.querySelectorAll("a");
+let body=document.body;
 
-nuit.addEventListener("click",function(e){
+
+
+nuit.addEventListener("click", function (e) {
+    a.forEach(function(a) {
+  if (a.classList.contains("anuit")) {
+    a.classList.remove("anuit");
+    nav.classList.remove("navnuit");
+    body.classList.remove("bodynuit");
+  } else {
+    a.classList.add("anuit");
+    nav.classList.add("navnuit");
+    body.classList.add("bodynuit");
     
-    if(a.classList.contains("anuit")){
-        a.classlist.remove("anuit")
-    }
-    else{a.classList.add("anuit") }
+  }
+});
+});
+
+
+let boutton=document.querySelector(".nuit")
+let navbar=document.querySelector("navbar")
+let lien=document.querySelectorAll("a")
+
+boutton.addEventListener("click", function (e) {
+    lien.forEach(function(lien) {
+  if (lien.classList.contains("liendark")) {
+    lien.classList.remove("liendark");
+    navbar.classList.remove("navbardark");
+
+  } else {
+    lien.classList.add("liendark");
+    navbar.classList.add("navbardark");
+
+
+  }
+});
 });
---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+let nuit = document.querySelector(".nuit");
+let body = document.body;
+
+
+// nuit.addEventListener("click", function (e) {
+//     a.forEach(function(a) {
+//   if (a.classList.contains("anuit")) {
+//     a.classList.remove("anuit");
+//     nav.classList.remove("navnuit");
+//   } else {
+//     a.classList.add("anuit");
+//     nav.classList.add("navnuit");
+    
+//   }
+// });
+// });
