f22f94a595b7bb7b05fcca876053652183edaebf
Lukassza <lukasszafranek@gmail.com>
2023-04-05 11:43:16 +0200

    8

--- public/js/base.js
+++ public/js/base.js
@@ -1,6 +1,7 @@
 let nuit = document.querySelector(".nuit");
 let nav = document.querySelector("nav");
 let a = document.querySelectorAll("a");
+let icon = document.querySelector(".far");
 let body=document.body;
 
 
@@ -11,10 +12,12 @@ nuit.addEventListener("click", function (e) {
     a.classList.remove("anuit");
     nav.classList.remove("navnuit");
     body.classList.remove("bodynuit");
+    icon.classList.remove("farnuit");
   } else {
     a.classList.add("anuit");
     nav.classList.add("navnuit");
     body.classList.add("bodynuit");
+    icon.classList.add("farnuit");
     
   }
 });
