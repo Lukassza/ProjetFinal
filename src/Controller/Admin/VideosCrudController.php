daef5da68e5332e9171c075bba60b4d96da4d314
Lukassza <lukasszafranek@gmail.com>
2023-04-11 16:09:37 +0200

    10

--- src/Controller/Admin/VideosCrudController.php
+++ src/Controller/Admin/VideosCrudController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Videos;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
@@ -15,14 +17,20 @@ class VideosCrudController extends AbstractCrudController
         return Videos::class;
     }
 
-   /* 
+   
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            
+            TextField::new('titre'),
+            TextField::new('lien'),
+            
+            ChoiceField::new('categorie')->setChoices([
+                'gtfo' => '1',
+                'lol' => '2',
+            ])
+            // https://stackoverflow.com/questions/53997251/symfony-forms-expected-argument-of-type-string-given-at-property-path
         ];
-    }*/
+    }
     
 }
